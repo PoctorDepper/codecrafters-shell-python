ef1418ac6c6594c9519bb8802b32e5d6e67f181e
Nathaniel Sunday <nathille333@gmail.com>
2024-06-05 16:34:03 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -19,9 +19,9 @@ def main():
             case "type":
                 sys.stdout.write(args[0])
                 if args[0] in builtin_commands:
-                    sys.stdout.write("is a shell builtin\n")
+                    sys.stdout.write(" is a shell builtin\n")
                 else:
-                    sys.stdout.write("not found\n")
+                    sys.stdout.write(" not found\n")
             case _:
                 sys.stdout.write(f"{command}: command not found\n")
         continue
