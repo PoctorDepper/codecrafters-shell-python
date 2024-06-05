50f0eae0478240db20802418d8d3c280a85f5649
Nathaniel Sunday <nathille333@gmail.com>
2024-06-05 16:33:40 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 
 
 def main():
+    builtin_commands = ["exit", "echo", "type"]
 
     while True:
         # Clear terminal
@@ -14,7 +15,13 @@ def main():
             case "exit" if args[0] == "0":
                 sys.exit(0)
             case "echo":
-                print(*args[0:])
+                print(*args)
+            case "type":
+                sys.stdout.write(args[0])
+                if args[0] in builtin_commands:
+                    sys.stdout.write("is a shell builtin\n")
+                else:
+                    sys.stdout.write("not found\n")
             case _:
                 sys.stdout.write(f"{command}: command not found\n")
         continue
