bf185a65e051b24e8f4658d43b31d345a43ba49b
Nathaniel Sunday <nathille333@gmail.com>
2024-06-05 16:56:18 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import sys
+import os
 
+builtin_commands = ["exit", "echo", "type"]
+paths = os.environ.get("PATH").split(":")
 
 def main():
-    builtin_commands = ["exit", "echo", "type"]
 
     while True:
         # Clear terminal
@@ -17,11 +19,18 @@ def main():
             case "echo":
                 print(*args)
             case "type":
-                sys.stdout.write(args[0])
+                command_path = None
+                for path in paths:
+                    if os.path.isfile(f"{path}/{args[0]}"):
+                        command_path = path
+                        break
+                sys.stdout.write(f"{args[0]} ")
                 if args[0] in builtin_commands:
-                    sys.stdout.write(" is a shell builtin\n")
+                    sys.stdout.write("is a shell builtin\n")
+                elif command_path:
+                    sys.stdout.write(f"is {command_path}")
                 else:
-                    sys.stdout.write(" not found\n")
+                    sys.stdout.write("not found\n")
             case _:
                 sys.stdout.write(f"{command}: command not found\n")
         continue
