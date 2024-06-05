5e2422aee2c408e5ab85caddc99fa9ee1c7111b3
Nathaniel Sunday <nathille333@gmail.com>
2024-06-05 16:21:38 -0500

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,12 +9,12 @@ def main():
         sys.stdout.flush()
 
         # Get user input
-        args = input().split(" ")
+        args = input().split()
         match command := args.pop(0):
             case "exit" if args[0] == "0":
                 sys.exit(0)
             case "echo":
-                print(" ".join(args))
+                print(args[0:])
             case _:
                 sys.stdout.write(f"{command}: command not found\n")
         continue
